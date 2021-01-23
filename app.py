750f55616f820d77846f10d2048d988a588d6831
Hamilton <hamilton@hts-net.com.br>
2021-01-23 20:52:48 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,67 @@
+# importando a biblioteca
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+
+df = pd.read_csv('dfSCS.csv')
+# trocando o nome do campo e ajustando o valor para tirar informação inexistente
+df['latitude'] = df['LAT_(GEO)'].apply(lambda x: float(x.replace(',', '.')) if x != 'NAO DISPONIVEL' else -23.62306)
+df['longitude'] = df['LONG_(GEO)'].apply(lambda x: float(x.replace(',', '.')) if x != 'NAO DISPONIVEL' else -46.55111)
+
+
+
+PAGE_CONFIG = {
+    'page_title': 'Acidentes São Caetano do Sul',
+    'page_icon': ':smiley',
+    'layout': 'centered'
+}
+#st.beta_set_page_config(**PAGE_CONFIG)
+
+def main():
+  # definindo os parâmetros
+    st.title('Acidentes automotivos em São Caetano do Sul')
+    st.markdown("""
+    Usando as informações do site oficial do **Estado de São Paulo**, 
+    este quadro apresenta detalhes dos acidentes registrados na
+    cidade de São Caetano do Sul
+    """)
+    # informação na side bar
+    st.sidebar.info('Foram carregadas {} linhas'. format(df.shape[0]))
+
+    if st.sidebar.checkbox('Ver dados de entrada'):
+        st.header('Dados de entrada')
+        st.write(df)
+
+    ano_selecionado = st.sidebar.slider('Selecione um ano', 2019, 2020, 2020)     # limite inferior, limite superior, valor default
+    df_selected = df[df['Ano do Acidente'] == ano_selecionado]
+
+    st.subheader('Mapa dos acidentes veiculares')
+    st.map(df_selected)
+
+    # st.pydeck_chart(pdk.Deck(
+    #     initial_view_state=pdk.ViewState(
+    #         latitude=-23.62306,
+    #         longitude=-46.55111,
+    #         zoom=12,
+    #         radius=20,
+    #         pitch=40
+    #     ),
+    #     layers=[
+    #         pdk.Layer(
+    #             'HexagonLayer',
+    #             data=df_selected[['latitude', 'longitude']],
+    #             get_position='[longitude, latitude]',
+    #             elevation_state=10,
+    #             pickable=True,
+    #             extruded=True,
+    #             get_color='[200, 30, 0, 160]',
+
+    #         )
+    #     ]
+    # ))
+    
+     
+
+
+if __name__ == '__main__':
+	main()  
---
+++ first_app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+st.title('My first app')
+
+st.write('Aqui eu posso escrever qualquer coisa!')
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write(df)
+
+"""
+# Aqui eu coloco um título
+
+E depois algum texto a mais
+"""
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.write(chart_data)     
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option    
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")  
+
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
